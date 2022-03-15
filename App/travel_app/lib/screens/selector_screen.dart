43eda2e67507fa5043dfb23c006e1da07ed7bfda
halo2612 <adityadev2612@gmail.com>
2022-03-15 18:43:53 +0530

    started hotel selector screen

--- App/travel_app/lib/screens/selector_screen.dart
+++ App/travel_app/lib/screens/selector_screen.dart
@@ -10,34 +10,84 @@ class _HotelSelectorState extends State<HotelSelector> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      children: [
-        Text("Hotel selector"),
-        Container(
-          height: MediaQuery.of(context).size.width,
-          child: Align(
-            alignment: Alignment.center,
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, AppRoutes.itineraryRoute);
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: Center(
+            child: InkWell(
+              onTap: () {
+                Navigator.pushNamed(context, AppRoutes.destinationRoute);
               },
-              child: Text("To itinerary page"),
+              child: Text(
+                "Location",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
             ),
           ),
+          toolbarHeight: 100,
+          leading: Padding(
+            padding: const EdgeInsets.fromLTRB(8.0, 0.0, 0, 0),
+            child: IconButton(
+              onPressed: () {
+                Navigator.pushNamed(context, AppRoutes.homeRoute);
+              },
+              //icon: Icon(Icons.home_filled),
+              icon: Image.asset("assets/LOGO.png"),
+              iconSize: 35,
+              color: Colors.white,
+            ),
+          ),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0.0, 0.0, 8.0, 0.0),
+              child: IconButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, AppRoutes.historyRoute);
+                },
+                icon: Icon(Icons.account_circle_outlined),
+                iconSize: 35,
+                color: Colors.white,
+              ),
+            ),
+          ],
+          backgroundColor: Colors.transparent,
+          //backgroundColor: Colors.red,
+          elevation: 0,
         ),
-        Container(
-          height: MediaQuery.of(context).size.width,
-          child: Align(
-            alignment: Alignment.center,
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, AppRoutes.hotelRoute);
-              },
-              child: Text("To Hotel Screen"),
-            ),
-          ),
-        )
-      ],
-    ));
+        extendBodyBehindAppBar: true,
+        body: Stack(
+          children: [
+            Positioned.fill(
+              top: 170,
+              child: Align(
+                alignment: Alignment.topCenter,
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(12.0, 0.0, 12.0, 0.0),
+                  child: Container(
+                    height: MediaQuery.of(context).size.width / 2.5,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(25),
+                      color: Colors.white30,
+                    ),
+                  ),
+                ),
+              ),
+            )
+            /* Container(
+              height: MediaQuery.of(context).size.width,
+              child: Align(
+                alignment: Alignment.center,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, AppRoutes.itineraryRoute);
+                  },
+                  child: Text("To itinerary page"),
+                ),
+              ),
+            ), */
+          ],
+        ));
   }
 }
