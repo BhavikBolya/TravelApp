d06c3149309b3f5286c7fcd656bfae8905fbc167
halo2612 <adityadev2612@gmail.com>
2022-03-19 00:13:33 +0530

    Completed preferences screen

--- App/travel_app/lib/screens/preferences_screen.dart
+++ App/travel_app/lib/screens/preferences_screen.dart
@@ -2,77 +2,275 @@ import 'package:flutter/material.dart';
 import 'package:travel_app/utils/routes.dart';
 
 class PreferencesScreen extends StatefulWidget {
+  const PreferencesScreen({Key? key}) : super(key: key);
+
   @override
-  _PreferencesScreenState createState() => _PreferencesScreenState();
+  State<PreferencesScreen> createState() => _PreferencesScreenState();
 }
 
 class _PreferencesScreenState extends State<PreferencesScreen> {
+  List userHotelPreferences = [];
+  List userCuisinePreferences = [];
+  List userAttractionPreferences = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.black,
-        appBar: AppBar(
-          title: Center(
-            child: InkWell(
-              onTap: () {
-                Navigator.pushNamed(context, AppRoutes.destinationRoute);
-              },
-              child: Text(
-                "Location",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Center(
+          child: InkWell(
+            onTap: () {
+              Navigator.pushNamed(context, AppRoutes.destinationRoute);
+            },
+            child: Text(
+              "Location",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
               ),
             ),
           ),
-          toolbarHeight: 100,
-          leading: Padding(
-            padding: const EdgeInsets.fromLTRB(8.0, 0.0, 0, 0),
+        ),
+        toolbarHeight: 100,
+        leading: Padding(
+          padding: const EdgeInsets.fromLTRB(8.0, 0.0, 0, 0),
+          child: IconButton(
+            onPressed: () {
+              Navigator.pushNamed(context, AppRoutes.homeRoute);
+            },
+            //icon: Icon(Icons.home_filled),
+            icon: Image.asset("assets/LOGO.png"),
+            iconSize: 35,
+            color: Colors.white,
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0.0, 0.0, 8.0, 0.0),
             child: IconButton(
               onPressed: () {
-                Navigator.pushNamed(context, AppRoutes.homeRoute);
+                Navigator.pushNamed(context, AppRoutes.historyRoute);
               },
-              //icon: Icon(Icons.home_filled),
-              icon: Image.asset("assets/LOGO.png"),
+              icon: Icon(Icons.account_circle_outlined),
               iconSize: 35,
               color: Colors.white,
             ),
           ),
-          actions: [
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0.0, 0.0, 8.0, 0.0),
-              child: IconButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, AppRoutes.historyRoute);
-                },
-                icon: Icon(Icons.account_circle_outlined),
-                iconSize: 35,
+        ],
+        backgroundColor: Colors.transparent,
+        //backgroundColor: Colors.red,
+        elevation: 0,
+      ),
+      body: Container(
+          child: Column(
+        children: [
+          Text(
+            "Hotel preferences",
+            style: TextStyle(
+                color: Colors.amber,
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.bold),
+          ),
+          Container(
+            height: 130,
+            color: Colors.transparent,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                ...List.generate(
+                    hotelPref.length,
+                    (index) => Item(
+                          preferences: hotelPref[index],
+                          onSelected: (bool value) {
+                            if (value) {
+                              userHotelPreferences.add(hotelPref[index].title);
+                              print(userHotelPreferences);
+                              print(userCuisinePreferences);
+                              print(userAttractionPreferences);
+                            } else {
+                              userHotelPreferences
+                                  .remove(hotelPref[index].title);
+                              print(userHotelPreferences);
+                              print(userCuisinePreferences);
+                              print(userAttractionPreferences);
+                            }
+
+                            setState(() {});
+                          },
+                        ))
+              ],
+            ),
+          ),
+          Text(
+            "Cuisine preferences",
+            style: TextStyle(
+                color: Colors.amber,
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.bold),
+          ),
+          Container(
+            height: 130,
+            color: Colors.transparent,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                ...List.generate(
+                    cuisinePref.length,
+                    (index) => Item(
+                          preferences: cuisinePref[index],
+                          onSelected: (bool value) {
+                            if (value) {
+                              userCuisinePreferences
+                                  .add(cuisinePref[index].title);
+                              print(userHotelPreferences);
+                              print(userCuisinePreferences);
+                              print(userAttractionPreferences);
+                            } else {
+                              userCuisinePreferences
+                                  .remove(cuisinePref[index].title);
+                              print(userHotelPreferences);
+                              print(userCuisinePreferences);
+                              print(userAttractionPreferences);
+                            }
+
+                            setState(() {});
+                          },
+                        ))
+              ],
+            ),
+          ),
+          Text(
+            "Attraction preferences",
+            style: TextStyle(
+                color: Colors.amber,
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.bold),
+          ),
+          Container(
+            height: 130,
+            color: Colors.transparent,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                ...List.generate(
+                    attractionsPref.length,
+                    (index) => Item(
+                          preferences: attractionsPref[index],
+                          onSelected: (bool value) {
+                            if (value) {
+                              userAttractionPreferences
+                                  .add(attractionsPref[index].title);
+                              print(userHotelPreferences);
+                              print(userCuisinePreferences);
+                              print(userAttractionPreferences);
+                            } else {
+                              userAttractionPreferences
+                                  .remove(attractionsPref[index].title);
+                              print(userHotelPreferences);
+                              print(userCuisinePreferences);
+                              print(userAttractionPreferences);
+                            }
+
+                            setState(() {});
+                          },
+                        ))
+              ],
+            ),
+          ),
+        ],
+      )),
+    );
+  }
+}
+
+class Item extends StatefulWidget {
+  final Preferences preferences;
+  final ValueChanged<bool> onSelected;
+  const Item({
+    Key? key,
+    required this.preferences,
+    required this.onSelected,
+  }) : super(key: key);
+
+  @override
+  State<Item> createState() => _ItemState();
+}
+
+class _ItemState extends State<Item> {
+  bool isSelected = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          isSelected = !isSelected;
+          widget.onSelected(isSelected);
+        });
+      },
+      child: Container(
+        margin: EdgeInsets.all(16),
+        child: Column(
+          children: [
+            AnimatedContainer(
+              duration: Duration(
+                milliseconds: 90,
+              ),
+              padding: EdgeInsets.all(16.0),
+              decoration: BoxDecoration(
+                  shape: BoxShape.rectangle,
+                  borderRadius: BorderRadius.circular(5),
+                  border: isSelected
+                      ? Border.all(
+                          width: 2,
+                          color: Colors.white,
+                        )
+                      : null),
+              child: Icon(
+                widget.preferences.icons,
+                size: 35,
                 color: Colors.white,
               ),
             ),
-          ],
-          backgroundColor: Colors.transparent,
-          //backgroundColor: Colors.red,
-          elevation: 0,
-        ),
-        extendBodyBehindAppBar: true,
-        body: Stack(
-          children: [
-            Text("Preferences screen"),
-            Container(
-              height: MediaQuery.of(context).size.width,
-              child: Align(
-                alignment: Alignment.center,
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, AppRoutes.hotelselectorRoute);
-                  },
-                  child: Text("To hotel selector"),
-                ),
-              ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              widget.preferences.title,
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
             )
           ],
-        ));
+        ),
+      ),
+    );
   }
 }
+
+class Preferences {
+  final String title;
+  final IconData icons;
+
+  Preferences(this.title, this.icons);
+}
+
+List<Preferences> hotelPref = [
+  Preferences("Spa", Icons.spa),
+  Preferences("Pool", Icons.pool),
+  Preferences("Parking", Icons.local_parking),
+  Preferences("Wifi", Icons.wifi),
+  Preferences("Breakfast", Icons.breakfast_dining),
+];
+
+List<Preferences> cuisinePref = [
+  Preferences("Italian ", Icons.local_pizza),
+  Preferences("Fastfood", Icons.fastfood),
+  Preferences("Chineese", Icons.food_bank),
+];
+
+List<Preferences> attractionsPref = [
+  Preferences("Tours", Icons.tour),
+  Preferences("Adventure", Icons.sports),
+  Preferences("History", Icons.attractions),
+];
