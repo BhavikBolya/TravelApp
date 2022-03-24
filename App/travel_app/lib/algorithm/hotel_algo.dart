7b5cfd98ed23326767f0fb385eacde3efc1ef2cc
halo2612 <adityadev2612@gmail.com>
2022-03-25 00:17:43 +0530

    fixed algo

--- App/travel_app/lib/algorithm/hotel_algo.dart
+++ App/travel_app/lib/algorithm/hotel_algo.dart
@@ -1,19 +1,11 @@
-
 import 'package:df/df.dart';
-import 'package:path_provider/path_provider.dart';
+
 import 'package:sortedmap/sortedmap.dart';
 import 'package:deep_collection/deep_collection.dart';
-import 'package:csv/csv.dart';
-//import 'package:ml_dataframe/ml_dataframe.dart';
-// ignore: import_of_legacy_library_into_null_safe
-import 'package:flutter/services.dart' show ByteData, rootBundle;
 
-Future<List> hotel_algo(String inputCity, int inputBudget, int inputDays,
+Future<List> hotel_algo(DataFrame df, String inputCity, int inputBudget, int inputDays,
     List inputAmenities) async {
-      
-  /* final yourString = await rootBundle.loadString('assets/database/hotels.csv');
-   List<List<dynamic>> dflist = const CsvToListConverter().convert(yourString); */
-  final df = await DataFrame.fromCsv('hotels.csv', verbose: true);
+  //final df = await DataFrame.fromCsv('hotels.csv', verbose: true);
   // String input_city = "Jaipur";
   // int input_budget = 500;
   // int input_days = 7;
@@ -24,115 +16,112 @@ Future<List> hotel_algo(String inputCity, int inputBudget, int inputDays,
   //   'Bar',
   //   'Parking included'
   // ];
-  // double bugdetperday = inputBudget / inputDays;
+  double bugdetperday = inputBudget / inputDays;
 
-  // List shortlist = [];
-  // for (int i = 0; i < df.length; i++) {
-  //   String stPrice =
-  //       df.colRecords('hotel_price', offset: i, limit: i + 1)[0].toString();
-  //   double dbPrice = double.parse(stPrice);
-  //   // ignore: unrelated_type_equality_checks
-  //   if (df.colRecords('city', offset: i, limit: i + 1)[0].toString() ==
-  //           inputCity &&
-  //       dbPrice < bugdetperday) {
-  //     shortlist.add(i);
-  //   }
-  // }
-  // List hotelScore = [];
+  List shortlist = [];
+  for (int i = 0; i < df.length; i++) {
+    String stPrice =
+        df.colRecords('hotel_price', offset: i, limit: i + 1)[0].toString();
+    double dbPrice = double.parse(stPrice);
+    // ignore: unrelated_type_equality_checks
+    if (df.colRecords('city', offset: i, limit: i + 1)[0].toString() ==
+            inputCity &&
+        dbPrice < bugdetperday) {
+      shortlist.add(i);
+    }
+  }
+  List hotelScore = [];
 
-  // for (int i = 0; i < shortlist.length; i++) {
-  //   int score = 0;
-  //   var amenityIter = inputAmenities.iterator;
-  //   String amn = df
-  //       .colRecords('amenities', offset: i, limit: i + 1)
-  //       .toString()
-  //       .replaceAll("[", "");
-  //   amn = amn.replaceAll("]", "");
-  //   amn = amn.replaceAll("'", "");
-  //   while (amenityIter.moveNext()) {
-  //     if (amn.contains(amenityIter.current)) {
-  //       score++;
-  //     }
-  //   }
-  //   hotelScore.add(score);
-  // }
-  // Map<Comparable, dynamic> shortlist2 = SortedMap(const Ordering.byValue());
+  for (int i = 0; i < shortlist.length; i++) {
+    int score = 0;
+    var amenityIter = inputAmenities.iterator;
+    String amn = df
+        .colRecords('amenities', offset: i, limit: i + 1)
+        .toString()
+        .replaceAll("[", "");
+    amn = amn.replaceAll("]", "");
+    amn = amn.replaceAll("'", "");
+    while (amenityIter.moveNext()) {
+      if (amn.contains(amenityIter.current)) {
+        score++;
+      }
+    }
+    hotelScore.add(score);
+  }
+  Map<Comparable, dynamic> shortlist2 = SortedMap(const Ordering.byValue());
 
-  // for (int i = 0; i < shortlist.length; i++) {
-  //   if (hotelScore[i] >= 2) {
-  //     shortlist2[shortlist[i]] = hotelScore[i];
-  //   }
-  // }
-  // shortlist2 = shortlist2.deepReverse();
-  // var shortlistedScores = <String, List>{
-  //   '5_hotels': [],
-  //   '4_hotels': [],
-  //   '3_hotels': [],
-  //   '2_hotels': []
-  // };
+  for (int i = 0; i < shortlist.length; i++) {
+    if (hotelScore[i] >= 2) {
+      shortlist2[shortlist[i]] = hotelScore[i];
+    }
+  }
+  shortlist2 = shortlist2.deepReverse();
+  var shortlistedScores = <String, List>{
+    '5_hotels': [],
+    '4_hotels': [],
+    '3_hotels': [],
+    '2_hotels': []
+  };
 
-  // for (MapEntry hotel in shortlist2.entries) {
-  //   if (hotel.value == 5) shortlistedScores['5_hotels']!.add(hotel.key);
-  //   if (hotel.value == 4) shortlistedScores['4_hotels']!.add(hotel.key);
-  //   if (hotel.value == 3) shortlistedScores['3_hotels']!.add(hotel.key);
-  //   if (hotel.value == 2) shortlistedScores['2_hotels']!.add(hotel.key);
-  // }
-  // List hotelRecc = [];
+  for (MapEntry hotel in shortlist2.entries) {
+    if (hotel.value == 5) shortlistedScores['5_hotels']!.add(hotel.key);
+    if (hotel.value == 4) shortlistedScores['4_hotels']!.add(hotel.key);
+    if (hotel.value == 3) shortlistedScores['3_hotels']!.add(hotel.key);
+    if (hotel.value == 2) shortlistedScores['2_hotels']!.add(hotel.key);
+  }
+  List hotelRecc = [];
 
-  // Map<Comparable, dynamic> hotels_5 = SortedMap(const Ordering.byValue());
-  // List h5 = shortlistedScores['5_hotels']!.toList(growable: false);
-  // for (int i in h5) {
-  //   hotels_5[i] = double.parse(
-  //       df.colRecords('hotel_rating', offset: i, limit: i + 1)[0].toString());
-  // }
-  // hotels_5 = hotels_5.deepReverse();
-  // for (var x in hotels_5.keys) {
-  //   hotelRecc.add(x);
-  // }
+  Map<Comparable, dynamic> hotels_5 = SortedMap(const Ordering.byValue());
+  List h5 = shortlistedScores['5_hotels']!.toList(growable: false);
+  for (int i in h5) {
+    hotels_5[i] = double.parse(
+        df.colRecords('hotel_rating', offset: i, limit: i + 1)[0].toString());
+  }
+  hotels_5 = hotels_5.deepReverse();
+  for (var x in hotels_5.keys) {
+    hotelRecc.add(x);
+  }
 
-  // Map<Comparable, dynamic> hotels_4 = SortedMap(const Ordering.byValue());
-  // List h4 = shortlistedScores['4_hotels']!.toList(growable: false);
-  // for (int i in h4) {
-  //   hotels_4[i] = double.parse(
-  //       df.colRecords('hotel_rating', offset: i, limit: i + 1)[0].toString());
-  // }
-  // hotels_4 = hotels_4.deepReverse();
-  // for (var x in hotels_4.keys) {
-  //   hotelRecc.add(x);
-  // }
+  Map<Comparable, dynamic> hotels_4 = SortedMap(const Ordering.byValue());
+  List h4 = shortlistedScores['4_hotels']!.toList(growable: false);
+  for (int i in h4) {
+    hotels_4[i] = double.parse(
+        df.colRecords('hotel_rating', offset: i, limit: i + 1)[0].toString());
+  }
+  hotels_4 = hotels_4.deepReverse();
+  for (var x in hotels_4.keys) {
+    hotelRecc.add(x);
+  }
 
-  // Map<Comparable, dynamic> hotels_3 = SortedMap(const Ordering.byValue());
-  // List h3 = shortlistedScores['3_hotels']!.toList(growable: false);
-  // for (int i in h3) {
-  //   hotels_3[i] = double.parse(
-  //       df.colRecords('hotel_rating', offset: i, limit: i + 1)[0].toString());
-  // }
-  // hotels_3 = hotels_3.deepReverse();
-  // for (var x in hotels_3.keys) {
-  //   hotelRecc.add(x);
-  // }
+  Map<Comparable, dynamic> hotels_3 = SortedMap(const Ordering.byValue());
+  List h3 = shortlistedScores['3_hotels']!.toList(growable: false);
+  for (int i in h3) {
+    hotels_3[i] = double.parse(
+        df.colRecords('hotel_rating', offset: i, limit: i + 1)[0].toString());
+  }
+  hotels_3 = hotels_3.deepReverse();
+  for (var x in hotels_3.keys) {
+    hotelRecc.add(x);
+  }
 
-  // Map<Comparable, dynamic> hotels_2 = SortedMap(const Ordering.byValue());
-  // List h2 = shortlistedScores['2_hotels']!.toList(growable: false);
-  // for (int i in h2) {
-  //   hotels_2[i] = double.parse(
-  //       df.colRecords('hotel_rating', offset: i, limit: i + 1)[0].toString());
-  // }
-  // hotels_2 = hotels_2.deepReverse();
-  // for (var x in hotels_2.keys) {
-  //   hotelRecc.add(x);
-  // }
+  Map<Comparable, dynamic> hotels_2 = SortedMap(const Ordering.byValue());
+  List h2 = shortlistedScores['2_hotels']!.toList(growable: false);
+  for (int i in h2) {
+    hotels_2[i] = double.parse(
+        df.colRecords('hotel_rating', offset: i, limit: i + 1)[0].toString());
+  }
+  hotels_2 = hotels_2.deepReverse();
+  for (var x in hotels_2.keys) {
+    hotelRecc.add(x);
+  }
 
-  // hotelRecc = hotelRecc.sublist(0, 3);
-  // var returnId = [];
-  // for (var hotelId in hotelRecc) {
-  //   returnId.add(int.parse(df
-  //       .colRecords('hotel_id', offset: hotelId, limit: hotelId + 1)[0]
-  //       .toString()));
-  // }
-  print(df);
-  //print(dflist);
+  hotelRecc = hotelRecc.sublist(0, 3);
   var returnId = [];
+  for (var hotelId in hotelRecc) {
+    returnId.add(int.parse(df
+        .colRecords('hotel_id', offset: hotelId, limit: hotelId + 1)[0]
+        .toString()));
+  }
   return returnId;
 }
 
@@ -140,5 +129,4 @@ Future<List> hotel_algo(String inputCity, int inputBudget, int inputDays,
   List out = await hotel_algo('Jaipur', 700, 5,
       ['Spa', 'Pool', 'Restaurant', 'Bar', 'Parking included']);
   print(out);
-}
- */
+} */
--- App/travel_app/lib/screens/preferences_screen.dart
+++ App/travel_app/lib/screens/preferences_screen.dart
@@ -1,13 +1,10 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:get/get_connect/http/src/http/io/file_decoder_io.dart';
-//import 'package:ml_dataframe/ml_dataframe.dart';
-import 'package:path_provider/path_provider.dart';
+
 import 'package:travel_app/algorithm/hotel_algo.dart';
 import 'package:travel_app/utils/routes.dart';
 import 'package:df/df.dart';
+import 'dart:convert';
 
 class PreferencesScreen extends StatefulWidget {
   const PreferencesScreen({Key? key}) : super(key: key);
@@ -20,21 +17,6 @@ class _PreferencesScreenState extends State<PreferencesScreen> {
   List userHotelPreferences = [];
   List userCuisinePreferences = [];
   List userAttractionPreferences = [];
-  Future<void> test() async {
-    //final rawCsvContent = await rootBundle.loadString('assets/database/hotels.csv');
-    String path = 'assets/database/hotels.csv';
-    //File file = File(path);
-    ByteData data = await rootBundle.load(path);
-    String directory = (await getTemporaryDirectory()).path;
-
-    //File file =  File(path).writeAsBytes(data, '$directory/uszips.csv');
-    File file = Future<File> writeAsBytes(data, FileMode mode = FileMode.append, bool flush = false);
-    final df = DataFrame.fromCsv(directory);
-    print(df);
-    /* hotel_algo('Jaipur', 700, 5,
-       ['Spa', 'Pool', 'Restaurant', 'Bar', 'Parking included']); */
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -205,8 +187,19 @@ class _PreferencesScreenState extends State<PreferencesScreen> {
           ),
           InkWell(
               onTap: () async {
-                // print(await hotel_algo("Jaipur", 700, 5, userHotelPreferences));
-                test();
+                var data =
+                    await rootBundle.loadString('assets/database/hotels.json');
+
+                var decodedData = List<Map<String, dynamic>>.from(
+                    json.decode(data)['hotels']);
+
+                DataFrame df = await DataFrame.fromRows(decodedData);
+
+                var hotelList = await hotel_algo(
+                    df, "Jaipur", 700, 5, userHotelPreferences);
+
+                print(df.subset(37, 38));
+
                 Navigator.pushNamed(context, AppRoutes.hotelselectorRoute);
               },
               child: Container(
